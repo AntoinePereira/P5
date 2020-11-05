6be20d5c06d741e15d11992822dd47874ae2d748
AntoinePereira <toine_42@hotmail.fr>
2020-11-05 13:48:44 +0100

    selecteur de couleurs page produit OK" "

--- js/produit.js
+++ js/produit.js
@@ -3,14 +3,15 @@ function getId(){
 	const id = param.replace("?id=", ""); 
 	return id;
 }
+const id = getId();
+console.log(id); 
 
-
-function addProduct(){
+function addProduct(tedInfo){
 	let divParent = document.getElementById('productContainer');
 
-	/*const img = document.createElement('img');
+	const img = document.createElement('img');
 	img.setAttribute('src', tedInfo.imageUrl);
-	divParent.appendChild(img);*/
+	divParent.appendChild(img);
 
 	const title = document.createElement('div');
 	title.innerHTML = tedInfo.name;
@@ -20,23 +21,27 @@ function addProduct(){
 	description.innerHTML = tedInfo.description;
 	divParent.appendChild(description);
 
-	const price = document.createElement('div');
-	price.innerHTML = tedInfo.price + $;
-
 	const colorSelector = document.createElement('select');
 	const defaultOption = document.createElement('option');
 	defaultOption.innerHTML = "Choisir la couleur";
 	colorSelector.appendChild(defaultOption);
 	divParent.appendChild(colorSelector);
 
+	tedInfo.colors.forEach((color) =>{
+		const colorOption = document.createElement('option');
+		colorOption.innerHTML = color;
+		colorSelector.appendChild(colorOption);
+
+	})
+
+	const price = document.createElement('div');
+	price.innerHTML = tedInfo.price + '$';
+	divParent.appendChild(price);
+
 	const btn = document.createElement('button');
 	btn.innerHTML = 'Ajouter au panier';
 	divParent.appendChild(btn);
 }
-
-const id = getId();
-console.log(id);
-
 const request = new XMLHttpRequest();
 
 request.onreadystatechange = function() {
