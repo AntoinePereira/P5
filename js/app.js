5afd482243111be7cbab1f7955e7303a050f752a
AntoinePereira <toine_42@hotmail.fr>
2020-11-05 18:50:04 +0100

    affichage ERROR if request fails

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const requestTeddies = new XMLHttpRequest();
 
+requestTeddies.open("GET", " http://localhost:3000/api/teddies/", true);
+
 requestTeddies.onreadystatechange = function() {
 	if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
 	  const teddies = JSON.parse(this.responseText);
-	  console.log(teddies);
-
+	 
 	  let divParent = document.getElementById('divJS');
 
 		teddies.forEach((item) => {
@@ -17,8 +18,13 @@ requestTeddies.onreadystatechange = function() {
 	    divParent.appendChild(divWrapper);
     })
   }
-};
+}
+requestTeddies.onerror = function(){
+	let divParent = document.getElementById('divJS');
+	divParent.innerHTML = "<h1>ERROR</h1>";
+}
 
+requestTeddies.send();
 
 function addTitle(item, divParent){
 	const title = document.createElement('h2');
@@ -36,34 +42,3 @@ function addImage(item, divParent){
 	image.src = item.imageUrl;
 	divParent.appendChild(image);
 }
-
-requestTeddies.open("GET", " http://localhost:3000/api/teddies/");
-requestTeddies.send();
-
-
-
-
-
-/* const api_url = " http://localhost:3000/api/teddies/";
-async function getTeddies(){
-	const response = await fetch(api_url);
-	const teddies = await response.json();
-	console.log(teddies);
-}
-getTeddies();
-
-
-
-//affiche ours-----------------------------------------------------------------------
-let divJS = document.getElementById('divjs1');
-	console.log(monDivPourJS1);
-monDivPourJS1.innerHTML = "<img src='http://localhost:3000/images/teddy_1.jpg'>";
-//affiche ours------------------------------------------------------------------------
-let img = document.createElement("img"); 
-img.src = "http://localhost:3000/images/teddy_1.jpg"; 
-let monDivPourJS2 = document.getElementById("divjs2"); 
-	console.log(img)
-monDivPourJS2.appendChild(img); 
-//-----------------------------------------------------------------------------------
-
-*/
